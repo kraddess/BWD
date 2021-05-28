3fb273e76c73b2fb9d81231112d69dfcb6f492a6
kraddess <kotentuoi1122@gmail.com>
2021-05-28 16:23:32 +0700

    add file

---
+++ JS/script.js
@@ -0,0 +1,63 @@
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: false,
+    autoplayTimeout:5000,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },
+      2000:{
+        item:5
+      }
+    }
+  })
+
+  function closeNav(event){
+    var con = document.getElementById("nav-container");
+    var cl = document.getElementById("close");
+    if(event.target == con || event.target == cl || event.target == cl.getElementsByTagName("i")[0])
+    {
+      con.style.display = "none";
+      document.getElementsByClassName("nav")[0].style.display = "none";
+    }
+     
+  }
+  function openNav(){
+    document.getElementsByClassName("nav")[0].style.display = "flex";
+    document.getElementById("nav-container").style.display = "flex";
+  }
+  function redirect(url){
+    window.location.href=url;
+  }
+// load more function
+  $(function () {
+    $(".post").slice(0, 3).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".post:hidden").slice(0, 3).slideDown();
+        if ($(".post:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+        
+    });
+});
+ 
+$('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
